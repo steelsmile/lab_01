00216050bdd48fa7dd5d6e5e70c7f58fae26be0d
Romanov Ilya <clockworld441@gmail.com>
2024-12-16 10:03:14 +0300

    поле 2

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ while(FrontIsClear() == true){
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/Double_beepers_01.w");
+    LoadWorld("worlds/Double_beepers_02.w");
     KarelProgram();
     Finish();
     return 0;
