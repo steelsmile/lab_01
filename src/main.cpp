d56e9b57b1a4f760ddfa5b900b29f62fb07cbe0b
Romanov Ilya <clockworld441@gmail.com>
2024-12-16 10:02:39 +0300

    поле 1

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 ///
 //==============================================================================
 
-
+#include <iostream>
 #include "karel.h"
 
 
@@ -13,16 +13,72 @@
 /// Программа для робота Карела
 ///
 void KarelProgram() {
-/// @todo Добавить программу для Карела
+  int p = 0;
 
+TurnLeft();
+
+while(FrontIsClear() == true){
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+    while(FrontIsClear() == true){
+        while(BeepersPresent() == true){
+            PickBeeper();
+            p += 2;
+        }
+        for(int i = 0; i < p; i++){
+            PutBeeper();
+        }
+        p = 0;
+        Move();
+    }
+    while(BeepersPresent() == true){
+        PickBeeper();
+        p += 2;
+    }
+    for(int i = 0; i < p; i++){
+        PutBeeper();
+    }
+    p = 0;
+    TurnLeft();
+    if (FrontIsClear() == true){
+        Move();
+        TurnLeft();
+       while(FrontIsClear() == true){
+            Move();
+        }
+    }
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
 }
-
-
+TurnLeft();
+TurnLeft();
+TurnLeft();
+while(FrontIsClear() == true){
+        while(BeepersPresent() == true){
+            PickBeeper();
+            p += 2;
+        }
+        for(int i = 0; i < p; i++){
+            PutBeeper();
+        }
+        p = 0;
+        Move();
+    }
+    while(BeepersPresent() == true){
+        PickBeeper();
+        p += 2;
+    }
+    for(int i = 0; i < p; i++){
+        PutBeeper();
+    }
+}
 //==============================================================================
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/.w");
+    LoadWorld("worlds/Double_beepers_01.w");
     KarelProgram();
     Finish();
     return 0;
